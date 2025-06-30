6955db14b6c800d894bde98bf8e8aede5d88e137
Jean Hyungjin Kim <jean.h.kim@polarisoffice.com>
2025-06-30 16:41:37 +0900

    햄버거 적용

--- js/nav.js
+++ js/nav.js
@@ -3,5 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     .then(response => response.text())
     .then(data => {
       document.getElementById("navbar").innerHTML = data;
+
+      const toggle = document.querySelector(".menu-toggle");
+      const navMenu = document.querySelector(".navbar");
+      if (toggle && navMenu) {
+        toggle.addEventListener("click", () => {
+          navMenu.classList.toggle("active");
+        });
+      }
     });
 });
